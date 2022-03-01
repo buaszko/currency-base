858710b81b378e06cc4513f7b8320a2ee697ac83
buaszko <blazej.nagiecki@gmail.com>
2022-03-01 22:56:19 +0100

    15.3 fix

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -16,30 +16,31 @@ describe('Component CurrencyForm', () => {
 
   for(const testObj of testCases) {
     it('should run action callback with proper data on form submit', () => {
-        const action = jest.fn();
-      
-        // render component
-        render(<CurrencyForm action={action} />);
-      
-        // find “convert” button
-        const submitButton = screen.getByText('Convert');
-      
-        // find fields elems
-        const amountField = screen.getByTestId('amount');
-        const fromField = screen.getByTestId('from-select');
-        const toField = screen.getByTestId('to-select');
-      
-        // set test values to fields
-        userEvent.type(amountField, '100');
-        userEvent.selectOptions(fromField, 'PLN');
-        userEvent.selectOptions(toField, 'USD');
-      
-        // simulate user click on "convert" button
-        userEvent.click(submitButton);
-      
-        // check if action callback was called once and with proper argument
-        expect(action).toHaveBeenCalledTimes(1);
-        expect(action).toHaveBeenCalledWith({ amount: 100, from: 'PLN', to: 'USD' });
+      const action = jest.fn();
+
+      // render component
+      render(<CurrencyForm action={action} />);
+
+      // find “convert” button
+      const submitButton = screen.getByText('Convert');
+
+      // find fields elems
+      const amountField = screen.getByTestId('amount');
+      const fromField = screen.getByTestId('from-select');
+      const toField = screen.getByTestId('to-select');
+
+      // set test values to fields
+      userEvent.type(amountField, testObj.amount);
+      userEvent.selectOptions(fromField, testObj.from);
+      userEvent.selectOptions(toField, testObj.to);
+
+      // simulate user click on "convert" button
+      userEvent.click(submitButton);
+
+      // check if action callback was called once and with proper argument
+      expect(action).toHaveBeenCalledTimes(1);
+      expect(action).toHaveBeenCalledWith({ amount: parseInt(testObj.amount), from: testObj.from, to: testObj.to });
+
       // unmount component
       cleanup()
     });
